7be9990ef119f3421074bf39e7780898fcf44cf7
Gabo Herrera Z <herzar_620@hotmail.com>
2023-12-09 23:20:51 +0100

    persist to zustate product store

--- src/store/useProductStore.tsx
+++ src/store/useProductStore.tsx
@@ -3,6 +3,7 @@ import { Filter } from "@/domain/model/filter";
 import { Product } from "@/domain/model/product";
 import { FilterProductUseCase } from "@/domain/useCase/product/filterProductUseCase";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface ProductState {
   products: Product[];
@@ -10,23 +11,31 @@ interface ProductState {
   filterProducts: (filter: Filter) => Promise<void>;
 }
 
-export const useProductStore = create<ProductState>((set) => {
-  const productImplementationRepository = new ProductImplementationRepository();
-  const filterProductUseCase = new FilterProductUseCase(
-    productImplementationRepository
-  );
+export const useProductStore = create(
+  persist<ProductState>(
+    (set) => {
+      const productImplementationRepository =
+        new ProductImplementationRepository();
+      const filterProductUseCase = new FilterProductUseCase(
+        productImplementationRepository
+      );
 
-  return {
-    products: [],
-    filter: {
-      categories: [],
-      subcategories: [],
-      price: [0, 10000],
-      orderBy: "asc",
+      return {
+        products: [],
+        filter: {
+          categories: [],
+          subcategories: [],
+          price: [0, 10000],
+          orderBy: "asc",
+        },
+        filterProducts: async (filter: Filter) => {
+          const products = await filterProductUseCase.execute(filter);
+          set(() => ({ products, filter }));
+        },
+      };
     },
-    filterProducts: async (filter: Filter) => {
-      const products = await filterProductUseCase.execute(filter);
-      set(() => ({ products, filter }));
-    },
-  };
-});
+    {
+      name: "product-storage",
+    }
+  )
+);
