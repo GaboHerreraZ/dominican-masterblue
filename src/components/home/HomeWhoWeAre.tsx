6436a9fa4f8c783b0a2314bb2415351529161445
Gabriel Herrera Z <gabriel@fmstms.com>
2024-05-25 09:21:07 -0500

    nueva pantalla de quienes somos

--- src/components/home/HomeWhoWeAre.tsx
+++ src/components/home/HomeWhoWeAre.tsx
@@ -1,23 +1,13 @@
 import Image from "next/image";
 import { getTranslation } from "@/i18n";
+import { Honestity, Reliability, Responsability } from "@/utils/icons";
 
 export const HomeWhoWeAre = async ({ lng }: { lng: string }) => {
   const { t } = await getTranslation(lng, "home");
 
   return (
-    <section className="flex border-l-4 border-gold  mx-auto max-w-[1000px] my-10 px-5  ">
-      <div className="w-full lg:w-2/3 ">
-        <div className="flex w-full">
-          <h1 className="text-8xl intersect:animate-fade-down intersect:animate-duration-700  w-1/2 font-bold text-gold ">
-            {t("whoWeAre")}
-          </h1>
-        </div>
-        <p className="mt-5 pr-5 text-lg intersect:animate-fade-down intersect:animate-duration-1000">
-          <span className="font-bold text-xl">{t("whoWeAreDescription1")}</span>
-          {t("whoWeAreDescription2")}
-        </p>
-      </div>
-      <div className="w-1/3 hidden md:block   relative intersect:animate-fade animate-duration-700 ">
+    <section className="flex w-100 h-[250px] md:h-[500px] my-10 z-10 justify-center m-auto container">
+      <div className="relative hidden md:grid md:w-1/3">
         <Image
           src="https://llfscfhwhopxywvxgwcz.supabase.co/storage/v1/object/public/publicImage/home/lamps.webp?t=2024-05-03T19%3A05%3A00.758Z"
           alt="Logo Dominican Masterblue"
@@ -25,6 +15,40 @@ export const HomeWhoWeAre = async ({ lng }: { lng: string }) => {
           sizes="100%"
           style={{ objectFit: "cover" }}
         />
+      </div>
+
+      <div className="w-full md:w-2/3 h-full flex items-center relative z-20">
+        <div className="flex absolute border-l-8 border-gold  md:-left-20 p-5 flex-col z-20  bg-white justify-center  mx-auto container shadow-lg ">
+          <h1 className="text-5xl intersect:animate-fade-down intersect:animate-duration-700 w-full md:w-1/2 font-bold text-gold ">
+            {t("whoWeAre")}
+          </h1>
+          <p className="mt-2 intersect:animate-fade-down intersect:animate-duration-1000">
+            <span className="font-bold">{t("whoWeAreDescription1")}</span>
+            {t("whoWeAreDescription2")}
+          </p>
+          {/*<div className="flex gap-10 mt-4 py-5">
+            <div className="grid justify-center">
+              <div className="h-14 w-14">
+                <Honestity />
+              </div>
+              <h6>Honestidad</h6>
+            </div>
+
+            <div className="grid justify-center">
+              <div className="flex justify-center h-14 w-14 ">
+                <Reliability />
+              </div>
+              <h6>Confiabilidad</h6>
+            </div>
+
+            <div className="grid justify-center">
+              <div className="h-14 w-14 ">
+                <Responsability />
+              </div>
+              <h6>Responsabilidad</h6>
+            </div>
+          </div>*/}
+        </div>
       </div>
     </section>
   );
